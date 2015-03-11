9cd61d3609ed35b38ae8fac240662297b72ca341
BamBalooon <k.bb@wp.pl>
2015-03-11 21:22:07 +0100

    RF# ContextMapping maps GenericContextProperties, Plugin uses GenericContextProperties

--- aware-ContextBroadcaster/src/main/java/com/aware/plugin/contextbroadcaster/Plugin.java
+++ aware-ContextBroadcaster/src/main/java/com/aware/plugin/contextbroadcaster/Plugin.java
@@ -1,6 +1,7 @@
 package com.aware.plugin.contextbroadcaster;
 
-import android.content.*;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Handler;
@@ -8,19 +9,10 @@ import android.util.Log;
 import com.aware.Aware;
 import com.aware.cdm.ContextMapping;
 import com.aware.cdm.ContextPropertyCreator;
-import com.aware.cdm.factory.CellValueRetriever;
-import com.aware.cdm.factory.ContextPropertyFactory;
-import com.aware.cdm.factory.GenericContextPropertyFactory;
 import com.aware.cdm.processor.ContextPropertyProcessor;
-import com.aware.cdm.broadcast.processor.ContextUpdateBroadcaster;
 import com.aware.cdm.property.ContextProperty;
-import com.aware.cdm.broadcast.property.ContextPropertyParcel;
-import com.aware.cdm.broadcast.receiver.ContextUpdateReceiver;
 import com.aware.plugin.contextbroadcaster.positioner.NewRecordsCursorPositioner;
-import com.aware.plugin.google.activity_recognition.Google_AR_Provider;
-import com.aware.providers.Locations_Provider;
 import com.aware.utils.Aware_Plugin;
-import com.google.common.collect.ImmutableMap;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +24,6 @@ public class Plugin extends Aware_Plugin {
     private static final String TAG = "AWARE::ContextBroadcaster";
     private final Handler contextChangeHandler = new Handler();
     private List<ContentObserver> contentObservers;
-    private BroadcastReceiver broadcastReceiver;
 
     @Override
     public void onCreate() {
@@ -49,8 +40,6 @@ public class Plugin extends Aware_Plugin {
         sendBroadcast(refresh);
 
         ContentResolver contentResolver = getContentResolver();
-        Context applicationContext = getApplicationContext();
-
         contentObservers = new ArrayList<>();
         for (Uri contentUri : ContextMapping.getInstance().getContextUriList()) {
             ContentObserver contextObserver = new ContextObserver<>(
@@ -58,52 +47,6 @@ public class Plugin extends Aware_Plugin {
                     contentUri,
                     NewRecordsCursorPositioner.createInstancePositionedAtEnd(contentUri, contentResolver),
                     new ContextPropertyCreator<>(ContextMapping.getInstance()),
-                    new ContextUpdateBroadcaster(applicationContext));
-            contentResolver.registerContentObserver(contentUri, true, contextObserver);
-            contentObservers.add(contextObserver);
-        }
-
-        ContextMapping<ContextProperty> contextMapping = new ContextMapping<>(ImmutableMap.<Uri, ContextPropertyFactory<ContextProperty>>builder()
-                .put(
-                        Google_AR_Provider.Google_Activity_Recognition_Data.CONTENT_URI,
-                        new GenericContextPropertyFactory(
-                                new CellValueRetriever(),
-                                Google_AR_Provider.AUTHORITY,
-                                ImmutableMap.<String, Class<?>>builder()
-                                        .put(Google_AR_Provider.Google_Activity_Recognition_Data._ID, Integer.class)
-                                        .put(Google_AR_Provider.Google_Activity_Recognition_Data.TIMESTAMP, Long.class)
-                                        .put(Google_AR_Provider.Google_Activity_Recognition_Data.DEVICE_ID, String.class)
-                                        .put(Google_AR_Provider.Google_Activity_Recognition_Data.ACTIVITY_NAME, String.class)
-                                        .put(Google_AR_Provider.Google_Activity_Recognition_Data.ACTIVITY_TYPE, Integer.class)
-                                        .put(Google_AR_Provider.Google_Activity_Recognition_Data.CONFIDENCE, Integer.class)
-                                        .put(Google_AR_Provider.Google_Activity_Recognition_Data.ACTIVITIES, String.class)
-                                        .build()))
-                .put(
-                        Locations_Provider.Locations_Data.CONTENT_URI,
-                        new GenericContextPropertyFactory(
-                                new CellValueRetriever(),
-                                Locations_Provider.AUTHORITY,
-                                ImmutableMap.<String,Class<?>>builder()
-                                        .put(Locations_Provider.Locations_Data._ID, Integer.class)
-                                        .put(Locations_Provider.Locations_Data.TIMESTAMP, Long.class)
-                                        .put(Locations_Provider.Locations_Data.DEVICE_ID, String.class)
-                                        .put(Locations_Provider.Locations_Data.LATITUDE, Double.class)
-                                        .put(Locations_Provider.Locations_Data.LONGITUDE, Double.class)
-                                        .put(Locations_Provider.Locations_Data.BEARING, Double.class)
-                                        .put(Locations_Provider.Locations_Data.SPEED, Double.class)
-                                        .put(Locations_Provider.Locations_Data.ALTITUDE, Double.class)
-                                        .put(Locations_Provider.Locations_Data.PROVIDER, String.class)
-                                        .put(Locations_Provider.Locations_Data.ACCURACY, Double.class)
-                                        .put(Locations_Provider.Locations_Data.LABEL, String.class)
-                                        .build())
-                )
-                .build());
-        for (Uri contentUri : contextMapping.getContextUriList()) {
-            ContentObserver contextObserver = new ContextObserver<>(
-                    contextChangeHandler,
-                    contentUri,
-                    NewRecordsCursorPositioner.createInstancePositionedAtEnd(contentUri, contentResolver),
-                    new ContextPropertyCreator<>(contextMapping),
                     new ContextPropertyProcessor<ContextProperty>() {
                         @Override
                         public void process(ContextProperty contextProperty) {
@@ -113,24 +56,16 @@ public class Plugin extends Aware_Plugin {
             contentResolver.registerContentObserver(contentUri, true, contextObserver);
             contentObservers.add(contextObserver);
         }
-
-        broadcastReceiver = new ContextUpdateReceiver(new ContextPropertyProcessor<ContextPropertyParcel>() {
-            @Override
-            public void process(ContextPropertyParcel contextPropertyParcel) {
-                Log.d(TAG, contextPropertyParcel.toString());
-            }
-        });
-        registerReceiver(broadcastReceiver, new IntentFilter(ContextUpdateBroadcaster.ACTION_AWARE_CONTEXT_UPDATE));
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        ContentResolver contentResolver = getContentResolver();
         for (ContentObserver contentObserver : contentObservers) {
-            getContentResolver().unregisterContentObserver(contentObserver);
+            contentResolver.unregisterContentObserver(contentObserver);
         }
         contentObservers = null;
-        unregisterReceiver(broadcastReceiver);
 
         Aware.setSetting(getApplicationContext(), Settings.STATUS_PLUGIN_CONTEXT_BROADCASTER, false);
         Intent refresh = new Intent(Aware.ACTION_AWARE_REFRESH);
--- aware-cdm/src/main/java/com/aware/cdm/ContextMapping.java
+++ aware-cdm/src/main/java/com/aware/cdm/ContextMapping.java
@@ -1,14 +1,12 @@
 package com.aware.cdm;
 
 import android.net.Uri;
-import com.aware.cdm.broadcast.factory.*;
-import com.aware.cdm.factory.*;
+import com.aware.cdm.factory.CellValueRetriever;
+import com.aware.cdm.factory.ContextPropertyFactory;
+import com.aware.cdm.factory.GenericContextPropertyFactory;
 import com.aware.cdm.property.ContextProperty;
-import com.aware.cdm.broadcast.property.ContextPropertyParcel;
 import com.aware.plugin.google.activity_recognition.Google_AR_Provider;
-import com.aware.plugin.openweather.Provider;
 import com.aware.providers.Locations_Provider;
-import com.aware.providers.WiFi_Provider;
 import com.google.common.collect.ImmutableMap;
 
 import java.util.Map;
@@ -19,15 +17,43 @@ import java.util.Set;
  * @param <CP> mapped ContextProperty type
  */
 public class ContextMapping<CP extends ContextProperty> {
-    private static ContextMapping<ContextPropertyParcel> INSTANCE;
-    public static ContextMapping<ContextPropertyParcel> getInstance() {
+    private static ContextMapping<ContextProperty> INSTANCE;
+    public static ContextMapping<ContextProperty> getInstance() {
         if (INSTANCE == null) {
-            INSTANCE = new ContextMapping<>(ImmutableMap.<Uri, ContextPropertyFactory<ContextPropertyParcel>>builder()
-                    .put(Locations_Provider.Locations_Data.CONTENT_URI, new LocationPropertyFactory())
-                    .put(Google_AR_Provider.Google_Activity_Recognition_Data.CONTENT_URI, new GoogleActivityRecognitionPropertyFactory())
-                    .put(Provider.OpenWeather_Data.CONTENT_URI, new OpenWeatherPropertyFactory())
-                    .put(WiFi_Provider.WiFi_Sensor.CONTENT_URI, new WifiSensorPropertyFactory())
-                    .put(WiFi_Provider.WiFi_Data.CONTENT_URI, new WifiDataPropertyFactory())
+            INSTANCE = new ContextMapping<>(ImmutableMap.<Uri, ContextPropertyFactory<ContextProperty>>builder()
+                    .put(
+                            Google_AR_Provider.Google_Activity_Recognition_Data.CONTENT_URI,
+                            new GenericContextPropertyFactory(
+                                    new CellValueRetriever(),
+                                    Google_AR_Provider.AUTHORITY,
+                                    ImmutableMap.<String, Class<?>>builder()
+                                            .put(Google_AR_Provider.Google_Activity_Recognition_Data._ID, Integer.class)
+                                            .put(Google_AR_Provider.Google_Activity_Recognition_Data.TIMESTAMP, Long.class)
+                                            .put(Google_AR_Provider.Google_Activity_Recognition_Data.DEVICE_ID, String.class)
+                                            .put(Google_AR_Provider.Google_Activity_Recognition_Data.ACTIVITY_NAME, String.class)
+                                            .put(Google_AR_Provider.Google_Activity_Recognition_Data.ACTIVITY_TYPE, Integer.class)
+                                            .put(Google_AR_Provider.Google_Activity_Recognition_Data.CONFIDENCE, Integer.class)
+                                            .put(Google_AR_Provider.Google_Activity_Recognition_Data.ACTIVITIES, String.class)
+                                            .build()))
+                    .put(
+                            Locations_Provider.Locations_Data.CONTENT_URI,
+                            new GenericContextPropertyFactory(
+                                    new CellValueRetriever(),
+                                    Locations_Provider.AUTHORITY,
+                                    ImmutableMap.<String, Class<?>>builder()
+                                            .put(Locations_Provider.Locations_Data._ID, Integer.class)
+                                            .put(Locations_Provider.Locations_Data.TIMESTAMP, Long.class)
+                                            .put(Locations_Provider.Locations_Data.DEVICE_ID, String.class)
+                                            .put(Locations_Provider.Locations_Data.LATITUDE, Double.class)
+                                            .put(Locations_Provider.Locations_Data.LONGITUDE, Double.class)
+                                            .put(Locations_Provider.Locations_Data.BEARING, Double.class)
+                                            .put(Locations_Provider.Locations_Data.SPEED, Double.class)
+                                            .put(Locations_Provider.Locations_Data.ALTITUDE, Double.class)
+                                            .put(Locations_Provider.Locations_Data.PROVIDER, String.class)
+                                            .put(Locations_Provider.Locations_Data.ACCURACY, Double.class)
+                                            .put(Locations_Provider.Locations_Data.LABEL, String.class)
+                                            .build())
+                    )
                     .build());
         }
         return INSTANCE;
