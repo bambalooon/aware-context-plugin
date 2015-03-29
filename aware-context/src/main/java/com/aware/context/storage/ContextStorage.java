34119122f9590fe933a406e9a2661e2176bab3bd
BamBalooon <k.bb@wp.pl>
2015-03-29 19:12:28 +0200

    RF# refactored ContextStorage interface, it now provides methods to set and get ContextProperty, also to get all ContextProperties and whole Context

--- aware-context/src/main/java/com/aware/context/storage/ContextStorage.java
+++ aware-context/src/main/java/com/aware/context/storage/ContextStorage.java
@@ -1,6 +1,9 @@
 package com.aware.context.storage;
 
 import com.aware.context.Context;
+import com.aware.context.property.ContextProperty;
+
+import java.util.Collection;
 
 /**
  * Name: ContextStorage
@@ -8,9 +11,11 @@ import com.aware.context.Context;
  * Date: 2015-03-22
  * Created by BamBalooon
  *
- * @param <CTX> Context type
+ * @param <CP> ContextProperty type
  */
-public interface ContextStorage<CTX extends Context>  {
-    CTX getContext();
-    void setContext(CTX context);
+public interface ContextStorage<CP extends ContextProperty>  {
+    CP getContextProperty(String contextPropertyId);
+    void setContextProperty(CP contextProperty);
+    Collection<CP> getContextProperties();
+    Context<CP> getContext();
 }
