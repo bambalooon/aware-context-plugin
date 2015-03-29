857c7ecc738af8417f05d30a158c32496883cb72
BamBalooon <k.bb@wp.pl>
2015-03-29 19:04:51 +0200

    RF# removed generic ID type from ContextProperty, set to String

--- aware-context/src/main/java/com/aware/context/property/ContextProperty.java
+++ aware-context/src/main/java/com/aware/context/property/ContextProperty.java
@@ -2,9 +2,7 @@ package com.aware.context.property;
 
 /**
  * Created by Krzysztof Balon on 2015-03-11.
- *
- * @param <ID> identifier type
  */
-public interface ContextProperty<ID> {
-    ID getId();
+public interface ContextProperty {
+    String getId();
 }
