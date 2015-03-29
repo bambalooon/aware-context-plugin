78e56d61cddca458cc844b59ce5ae94a4b3aac73
BamBalooon <k.bb@wp.pl>
2015-03-29 19:07:29 +0200

    RF# added additional constructor to Context and added equals and hashcode methods to Context and GenericContextProperty, GenericContextProperty checks if constructors args aren't null

--- aware-context/src/main/java/com/aware/context/Context.java
+++ aware-context/src/main/java/com/aware/context/Context.java
@@ -13,20 +13,44 @@ import java.util.Map;
  * Created by BamBalooon
  *
  * @param <CP> stored ContextProperties type
- * @param <ID> stored ContextProperties identifier type
  */
-public class Context<CP extends ContextProperty<ID>, ID> {
-    private Map<ID, CP> contextProperties = Maps.newHashMap();
+public class Context<CP extends ContextProperty> {
+    private Map<String, CP> contextProperties = Maps.newHashMap();
+
+    public Context() {}
+
+    public Context(Collection<CP> contextProperties) {
+        for (CP contextProperty : contextProperties) {
+            this.contextProperties.put(contextProperty.getId(), contextProperty);
+        }
+    }
 
     public Collection<CP> getContextProperties() {
         return contextProperties.values();
     }
 
-    public CP getContextProperty(ID contextPropertyId) {
+    public CP getContextProperty(String contextPropertyId) {
         return contextProperties.get(contextPropertyId);
     }
 
-    public void setContextProperty(CP contextProperty) {
+    public void addContextProperty(CP contextProperty) {
         contextProperties.put(contextProperty.getId(), contextProperty);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Context)) return false;
+
+        Context context = (Context) o;
+
+        if (!contextProperties.equals(context.contextProperties)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return contextProperties.hashCode();
     }
 }
--- aware-context/src/main/java/com/aware/context/property/GenericContextProperty.java
+++ aware-context/src/main/java/com/aware/context/property/GenericContextProperty.java
@@ -2,6 +2,7 @@ package com.aware.context.property;
 
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.FluentIterable;
 
 import java.util.Map;
@@ -9,11 +10,13 @@ import java.util.Map;
 /**
  * Created by BamBalooon on 2015-03-10.
  */
-public class GenericContextProperty implements ContextProperty<String> {
+public class GenericContextProperty implements ContextProperty {
     private final String id;
     private final Map<String, Object> attributes;
 
     public GenericContextProperty(String id, Map<String, Object> attributes) {
+        Preconditions.checkNotNull(id, "GenericContextProperty ID must be specified.");
+        Preconditions.checkNotNull(attributes, "GenericContextProperty attributes map must be specified.");
         this.id = id;
         this.attributes = attributes;
     }
@@ -39,5 +42,25 @@ public class GenericContextProperty implements ContextProperty<String> {
                             }
                         }).join(Joiner.on(", "))
                 + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GenericContextProperty)) return false;
+
+        GenericContextProperty that = (GenericContextProperty) o;
+
+        if (!attributes.equals(that.attributes)) return false;
+        if (!id.equals(that.id)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + attributes.hashCode();
+        return result;
     }
 }
