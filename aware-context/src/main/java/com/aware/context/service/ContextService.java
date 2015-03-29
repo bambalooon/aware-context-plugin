47d98a662c86ae5aebc59ec8892a59b2191e1fda
BamBalooon <k.bb@wp.pl>
2015-03-29 19:10:09 +0200

    RF# removed ACTION_CONTEXT_RETRIEVE handling from ContextService

--- aware-context/src/main/java/com/aware/context/service/ContextService.java
+++ aware-context/src/main/java/com/aware/context/service/ContextService.java
@@ -1,13 +1,7 @@
 package com.aware.context.service;
 
-import android.app.Activity;
 import android.app.IntentService;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import com.aware.context.management.ContextManagement;
-import com.aware.context.wrapper.GenericContextWrapper;
 
 /**
  * Name: ContextService
@@ -17,8 +11,6 @@ import com.aware.context.wrapper.GenericContextWrapper;
  */
 public class ContextService extends IntentService {
     private static final String TAG = "ContextService";
-    public static final String ACTION_CONTEXT_RETRIEVE = "ACTION_CONTEXT_RETRIEVE";
-    public static final String CONTEXT_RECEIVER = "CONTEXT_RECEIVER";
 
     public ContextService() {
         super(TAG);
@@ -27,19 +19,6 @@ public class ContextService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         switch (intent.getAction()) {
-            case ACTION_CONTEXT_RETRIEVE:
-                PendingIntent contextReceiver = intent.getParcelableExtra(CONTEXT_RECEIVER);
-
-                Context applicationContext = getApplicationContext();
-                ContextManagement contextManagement = new ContextManagement(applicationContext);
-
-                Intent contextIntent = new GenericContextWrapper().wrapContext(contextManagement.getContext());
-                try {
-                    contextReceiver.send(applicationContext, Activity.RESULT_OK, contextIntent);
-                } catch (PendingIntent.CanceledException e) {
-                    Log.d(TAG, e.getMessage(), e);
-                }
-                break;
             default:
                 throw new IllegalArgumentException(
                         "Service invoked with unknown action: '" + intent.getAction() + "'.");
