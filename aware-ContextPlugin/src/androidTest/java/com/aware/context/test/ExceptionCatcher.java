f758f94d09569efafab4bfa62207d582abda9945
BamBalooon <k.bb@wp.pl>
2015-04-12 14:06:04 +0200

    SC# created ExceptionCatcher class to cleanly get thrown exception for testing purposes, fest-assert is impossible to use right now with Android: https://jira.codehaus.org/browse/FEST-485

---
+++ aware-ContextPlugin/src/androidTest/java/com/aware/context/test/ExceptionCatcher.java
@@ -0,0 +1,23 @@
+package com.aware.context.test;
+
+/**
+ * Name: ExceptionCatcher
+ * Description: ExceptionCatcher allows to clean and simple get of thrown exception
+ * Class created because it isn't possible to use fest-assert with Android,
+ * because fest-assert has dependency on java's awt, which is unknown to Android
+ * https://jira.codehaus.org/browse/FEST-485
+ * Date: 2015-04-12
+ * Created by BamBalooon
+ */
+public abstract class ExceptionCatcher {
+    public Exception catchException() {
+        try {
+            invoke();
+        } catch (Exception e) {
+            return e;
+        }
+        return null;
+    }
+
+    protected abstract void invoke() throws Exception;
+}
