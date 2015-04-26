f1f354ae910aaa2d0701206b99594a3bebcb0ba4
BamBalooon <k.bb@wp.pl>
2015-04-26 12:29:31 +0200

    RF# PoiRecommenderData constructor now takes context, adds DEVICE_ID to Contexts table, fixed bug of casting Double to int

--- aware-poirecommender/src/main/java/com/aware/poirecommender/provider/PoiRecommenderData.java
+++ aware-poirecommender/src/main/java/com/aware/poirecommender/provider/PoiRecommenderData.java
@@ -2,6 +2,9 @@ package com.aware.poirecommender.provider;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
+import com.aware.Aware;
+import com.aware.Aware_Preferences;
 import com.aware.context.property.GenericContextProperty;
 
 import java.util.Collection;
@@ -15,19 +18,22 @@ import java.util.Date;
  */
 public class PoiRecommenderData {
     public static final String CONTEXT_PROPERTY_ID_ATTRIBUTE = "_id";
+    private final Context context;
     private final ContentResolver contentResolver;
 
-    public PoiRecommenderData(ContentResolver contentResolver) {
-        this.contentResolver = contentResolver;
+    public PoiRecommenderData(Context context) {
+        this.context = context;
+        this.contentResolver = context.getContentResolver();
     }
 
     public void setContext(Collection<GenericContextProperty> contextProperties) {
         ContentValues values = new ContentValues();
         values.put(PoiRecommenderContract.Contexts.TIMESTAMP, new Date().getTime());
+        values.put(PoiRecommenderContract.Contexts.DEVICE_ID, Aware.getSetting(context, Aware_Preferences.DEVICE_ID));
         for (GenericContextProperty contextProperty : contextProperties) {
-            values.put(
-                    contextProperty.getId(),
-                    (int) contextProperty.getAttributes().get(CONTEXT_PROPERTY_ID_ATTRIBUTE));
+            Double contextPropertyIdAttribute = (Double) contextProperty.getAttributes()
+                    .get(CONTEXT_PROPERTY_ID_ATTRIBUTE);
+            values.put(contextProperty.getId(), contextPropertyIdAttribute.intValue());
         }
         contentResolver.insert(PoiRecommenderContract.Contexts.CONTENT_URI, values);
     }
