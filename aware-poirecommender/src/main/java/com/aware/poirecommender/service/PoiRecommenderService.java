91c35bc86e0b4f13d37ab660afc305a3d0f7c118
BamBalooon <k.bb@wp.pl>
2015-05-01 16:49:12 +0200

    RF# created second test implementation of PoiRecommenderService handling store poi with context action

--- aware-poirecommender/src/main/java/com/aware/poirecommender/service/PoiRecommenderService.java
+++ aware-poirecommender/src/main/java/com/aware/poirecommender/service/PoiRecommenderService.java
@@ -6,7 +6,9 @@ import android.util.Log;
 import com.aware.context.property.GenericContextProperty;
 import com.aware.context.provider.Context;
 import com.aware.context.transform.ContextPropertySerialization;
+import com.aware.poirecommender.openstreetmap.model.response.Element;
 import com.aware.poirecommender.provider.PoiRecommenderData;
+import com.aware.poirecommender.transform.Serializer;
 
 /**
  * Name: PoiRecommenderService
@@ -17,6 +19,7 @@ import com.aware.poirecommender.provider.PoiRecommenderData;
 public class PoiRecommenderService extends IntentService {
     public static final String ACTION_STORE_POI_WITH_CONTEXT =
             "com.aware.poirecommender.service.PoiRecommenderService.ACTION_STORE_POI_WITH_CONTEXT";
+    public static final String POI_EXTRA = "POI_EXTRA";
     private static final String TAG = "PoiRecommenderService";
 
     public PoiRecommenderService() {
@@ -28,9 +31,15 @@ public class PoiRecommenderService extends IntentService {
         switch (intent.getAction()) {
             case ACTION_STORE_POI_WITH_CONTEXT:
                 Log.d(TAG, "Storing current context in database...");
-                //FIXME: get sent POI
-                Context context = new Context(getContentResolver(), new ContextPropertySerialization<>(GenericContextProperty.class));
-                new PoiRecommenderData(getApplicationContext()).setContext(context.getContextProperties().values());
+                String poiJson = intent.getStringExtra(POI_EXTRA);
+                PoiRecommenderData poiRecommenderData = new PoiRecommenderData(getApplicationContext());
+                Serializer<Element> elementSerializer = new Serializer<>(Element.class);
+                if (poiJson != null) {
+                    poiRecommenderData.addElement(elementSerializer.deserialize(poiJson));
+                    Context context = new Context(getContentResolver(),
+                            new ContextPropertySerialization<>(GenericContextProperty.class));
+                    poiRecommenderData.addContext(context.getContextProperties().values());
+                }
                 break;
             default:
                 throw new IllegalArgumentException(
