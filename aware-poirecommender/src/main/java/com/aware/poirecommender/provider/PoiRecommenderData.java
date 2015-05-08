ecac77dc3519fc7e28d7b6277fc2bc3202097533
BamBalooon <k.bb@wp.pl>
2015-05-08 22:05:10 +0200

    RF# refactored PoiRecommenderData class addContextAndElement method, now it uses applyBatch to insert in one batch: poi, tags and corresponding context

--- aware-poirecommender/src/main/java/com/aware/poirecommender/provider/PoiRecommenderData.java
+++ aware-poirecommender/src/main/java/com/aware/poirecommender/provider/PoiRecommenderData.java
@@ -1,14 +1,14 @@
 package com.aware.poirecommender.provider;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
+import android.content.*;
 import android.database.Cursor;
+import android.os.RemoteException;
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
 import com.aware.context.property.GenericContextProperty;
 import com.aware.poirecommender.openstreetmap.model.response.Element;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 
@@ -28,65 +28,81 @@ public class PoiRecommenderData {
         this.contentResolver = context.getContentResolver();
     }
 
-    public void addContextAndElement(Collection<GenericContextProperty> contextProperties, Element element) {
-        if (!elementExists(element)) {
-            contentResolver.insert(PoiRecommenderContract.Pois.CONTENT_URI, generateElementValues(element));
+    public void addContextAndElement(Collection<GenericContextProperty> contextProperties, Element element)
+            throws RemoteException, OperationApplicationException {
+
+        String deviceId = Aware.getSetting(context, Aware_Preferences.DEVICE_ID);
+        long poiId = element.getId();
+
+        ArrayList<ContentProviderOperation> operations = new ArrayList<>();
+        if (!poiExists(poiId)) {
+            operations.add(generatePoiInsertOperation(element, deviceId));
+            operations.addAll(generatePoiTagsInsertOperations(element.getTags(), deviceId, poiId));
         }
 
-        ContentValues contextValues = generateContextValues(contextProperties);
-        contextValues.put(PoiRecommenderContract.Contexts.POI_ID, element.getId());
-        contentResolver.insert(PoiRecommenderContract.Contexts.CONTENT_URI, contextValues);
+        operations.add(generateContextInsertOperation(contextProperties, deviceId, poiId));
+        contentResolver.applyBatch(PoiRecommenderContract.AUTHORITY, operations);
     }
 
-    private ContentValues generateContextValues(Collection<GenericContextProperty> contextProperties) {
+    private ContentProviderOperation generateContextInsertOperation(
+            Collection<GenericContextProperty> contextProperties, String deviceId, long poiId) {
         ContentValues values = new ContentValues();
         values.put(PoiRecommenderContract.Contexts.TIMESTAMP, System.currentTimeMillis());
-        values.put(PoiRecommenderContract.Contexts.DEVICE_ID, Aware.getSetting(context, Aware_Preferences.DEVICE_ID));
+        values.put(PoiRecommenderContract.Contexts.DEVICE_ID, deviceId);
+        values.put(PoiRecommenderContract.Contexts.POI_ID, poiId);
         for (GenericContextProperty contextProperty : contextProperties) {
             Long contextPropertyIdAttribute = Long
                     .parseLong((String) contextProperty.getAttributes().get(CONTEXT_PROPERTY_TIMESTAMP_ATTRIBUTE));
             values.put(contextProperty.getId(), contextPropertyIdAttribute);
         }
-        return values;
+        return ContentProviderOperation
+                .newInsert(PoiRecommenderContract.Contexts.CONTENT_URI)
+                .withValues(values)
+                .build();
     }
 
-    private ContentValues generateElementValues(Element element) {
+    private ContentProviderOperation generatePoiInsertOperation(Element element, String deviceId) {
         ContentValues values = new ContentValues();
-        values.put(PoiRecommenderContract.Contexts.TIMESTAMP, System.currentTimeMillis());
-        values.put(PoiRecommenderContract.Contexts.DEVICE_ID, Aware.getSetting(context, Aware_Preferences.DEVICE_ID));
+        values.put(PoiRecommenderContract.Pois.TIMESTAMP, System.currentTimeMillis());
+        values.put(PoiRecommenderContract.Pois.DEVICE_ID, deviceId);
         values.put(PoiRecommenderContract.Pois.POI_ID, element.getId());
         values.put(PoiRecommenderContract.Pois.TYPE, element.getType());
         values.put(PoiRecommenderContract.Pois.LATITUDE, element.getLat());
         values.put(PoiRecommenderContract.Pois.LONGITUDE, element.getLon());
-        Map<String, String> tags = element.getTags();
-        if (tags != null) {
-            //TODO: change to new Tags table
-            values.put(PoiRecommenderContract.Pois.NAME, tags.get(Element.ELEMENT_NAME_TAG));
-//            values.put(PoiRecommenderContract.Pois.NAME, tags.getName());
-//            values.put(PoiRecommenderContract.Pois.LAYER, tags.getLayer());
-//            values.put(PoiRecommenderContract.Pois.OPENING_HOURS, tags.getOpeningHours());
-//            values.put(PoiRecommenderContract.Pois.PHONE, tags.getPhone());
-//            values.put(PoiRecommenderContract.Pois.WEBSITE, tags.getWebsite());
-//            values.put(PoiRecommenderContract.Pois.OPERATOR, tags.getOperator());
-//            values.put(PoiRecommenderContract.Pois.AMENITY, tags.getAmenity());
-//            values.put(PoiRecommenderContract.Pois.TOURISM, tags.getTourism());
-//            values.put(PoiRecommenderContract.Pois.SHOP, tags.getShop());
-//            values.put(PoiRecommenderContract.Pois.CITY, tags.getCity());
-//            values.put(PoiRecommenderContract.Pois.COUNTRY, tags.getCountry());
-//            values.put(PoiRecommenderContract.Pois.STREET, tags.getStreet());
-//            values.put(PoiRecommenderContract.Pois.HOUSE_NUMBER, tags.getHouseNumber());
-//            values.put(PoiRecommenderContract.Pois.HOUSE_NAME, tags.getHouseName());
-//            values.put(PoiRecommenderContract.Pois.POST_CODE, tags.getPostCode());
-        }
-        return values;
+        return ContentProviderOperation
+                .newInsert(PoiRecommenderContract.Pois.CONTENT_URI)
+                .withValues(values)
+                .build();
     }
 
-    private boolean elementExists(Element element) {
+    private Collection<ContentProviderOperation> generatePoiTagsInsertOperations(
+            Map<String, String> tags, String deviceId, long poiId) {
+        ArrayList<ContentProviderOperation> operations = new ArrayList<>();
+        if (tags != null) {
+            ContentValues baseValues = new ContentValues();
+            baseValues.put(PoiRecommenderContract.PoiTags.DEVICE_ID, deviceId);
+            baseValues.put(PoiRecommenderContract.PoiTags.POI_ID, poiId);
+            long timestamp = System.currentTimeMillis();
+            for (Map.Entry<String, String> tag : tags.entrySet()) {
+                ContentValues values = new ContentValues(baseValues);
+                values.put(PoiRecommenderContract.PoiTags.TIMESTAMP, timestamp++);
+                values.put(PoiRecommenderContract.PoiTags.KEY, tag.getKey());
+                values.put(PoiRecommenderContract.PoiTags.VALUE, tag.getValue());
+                operations.add(ContentProviderOperation
+                        .newInsert(PoiRecommenderContract.PoiTags.CONTENT_URI)
+                        .withValues(values)
+                        .build());
+            }
+        }
+        return operations;
+    }
+
+    private boolean poiExists(long poiId) {
         Cursor cursor = contentResolver.query(
                 PoiRecommenderContract.Pois.CONTENT_URI,
                 new String[]{PoiRecommenderContract.Pois._ID},
                 PoiRecommenderContract.Pois.POI_ID + "=?",
-                new String[]{Long.toString(element.getId())},
+                new String[]{Long.toString(poiId)},
                 null);
         try {
             return cursor.moveToFirst();
