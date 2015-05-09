21137da9ada9b805bb177c07fcf4f5bcec0d5d67
BamBalooon <k.bb@wp.pl>
2015-05-09 19:32:58 +0200

    RF# renamed action to store poi with context, added additional extra holding rating as double number, if poi or rating extra wasn't provided, then data won't be stored.

--- aware-poirecommender/src/main/java/com/aware/poirecommender/service/PoiRecommenderService.java
+++ aware-poirecommender/src/main/java/com/aware/poirecommender/service/PoiRecommenderService.java
@@ -20,9 +20,10 @@ import com.aware.poirecommender.transform.Serializer;
  */
 public class PoiRecommenderService extends IntentService {
     private static final String TAG = PoiRecommenderService.class.getSimpleName();
-    public static final String ACTION_STORE_POI_WITH_CONTEXT =
-            "com.aware.poirecommender.service.PoiRecommenderService.ACTION_STORE_POI_WITH_CONTEXT";
+    public static final String ACTION_STORE_AND_RATE_POI_WITH_CONTEXT =
+            "com.aware.poirecommender.service.PoiRecommenderService.ACTION_STORE_AND_RATE_POI_WITH_CONTEXT";
     public static final String POI_EXTRA = "POI_EXTRA";
+    public static final String RATING_EXTRA = "RATING_EXTRA";
 
     public PoiRecommenderService() {
         super(TAG);
@@ -31,16 +32,17 @@ public class PoiRecommenderService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         switch (intent.getAction()) {
-            case ACTION_STORE_POI_WITH_CONTEXT:
-                Log.d(TAG, "Storing current context in database...");
+            case ACTION_STORE_AND_RATE_POI_WITH_CONTEXT:
                 String poiJson = intent.getStringExtra(POI_EXTRA);
-                if (poiJson != null) {
+                double poiRating = intent.getDoubleExtra(RATING_EXTRA, -1);
+                if (poiJson != null && poiRating != -1) {
+                    Log.d(TAG, "Storing current context in database...");
                     Context context = new Context(getContentResolver(),
                             new ContextPropertySerialization<>(GenericContextProperty.class));
                     Element poiElement = new Serializer<>(Element.class).deserialize(poiJson);
                     try {
                         new PoiRecommenderData(getApplicationContext())
-                                .addContextAndElement(context.getContextProperties().values(), poiElement);
+                                .storeAndRate(context.getContextProperties().values(), poiElement, poiRating);
                     } catch (RemoteException | OperationApplicationException e) {
                         Log.e(TAG, "Exception thrown while inserting context and element to database.", e);
                     }
